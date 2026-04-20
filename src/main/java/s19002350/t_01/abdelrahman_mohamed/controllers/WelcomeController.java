ee957037b59883e5b8990beaee7a583862b4e8e9
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2026-04-20 22:57:30 +0200

    feat: add welcome endpoint

---
+++ src/main/java/s19002350/t_01/abdelrahman_mohamed/controllers/WelcomeController.java
@@ -0,0 +1,20 @@
+package s19002350.t_01.abdelrahman_mohamed.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${USER_NAME:Abdelrahman_Mohamed_Local}")
+    private String userName;
+
+    @Value("${ID:19002350}")
+    private String id;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello " + userName + " " + id + ", from Notes API";
+    }
+}
